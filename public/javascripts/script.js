b3ec705a6189e14d61277c5facd72f2c7a90b16f
Nouri T <zorisse@gmail.com>
2019-04-23 10:38:31 +0200

    adding D in tables

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,13 +1,13 @@
 // mise en place de la strtucture , on crée une array avec 52 semaines 
 let semaines = [];
-for (let i = 0; i < 52; i++) {
+for (let i = 0; i < 53; i++) {
   semaines.push(i);
 }
 
 // on crée les colonnes du tableaux 
-let columns = [{ title: "Dev" }]
+let columns_Name = [{ title: "Dev" }]
 for (let i = 1; i < semaines.length; i++) {
-  columns.push({ title: `Week ${i}` })
+  columns_Name.push({ title: `Week ${i}` })
 }
 // console.log('colonne', columns); ok ca marche 
 
@@ -15,9 +15,9 @@ for (let i = 1; i < semaines.length; i++) {
 
 
 
-
 document.addEventListener('DOMContentLoaded', () => {
   console.log('IronGenerator JS imported successfully!');
+
   // promise de tout recupérer 
   Promise.all([axios.get('/api/users'), axios.get('/api/dashboard')])
 
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
       console.log('array users', users)
 
 
-      for (let index = 0; index < users.length - 1; index++) {
+      for (let index = 0; index < users.length; index++) {
 
         //ajoute le nom du USER en debut de l'array 
         dataSet.push([`${users[index]}`])
@@ -37,17 +37,16 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log('test de la loop pour ' + users[index])
         console.log(oneUser)
 
-        // Loop pour chaque week ajouter un string à DATASET [USER][0, .... -> 50]
-        for (let i = 1; i < 53; i++) {
+        // Loop pour chaque week ajouter un string à DATASET [USER][0, .... -> 52]
+        for (let i = 1; i <= 52; i++) {
 
           let oneweek = oneUser.filter(e => e.week === i)
-
           let times = 0
           if (oneweek.length > 0) {
             oneweek.forEach(e => times += e.time)
             console.log('times ', times)
           }
-          dataSet[index][i] = times.toFixed(2) + ' H'
+          dataSet[index][i] = times.toFixed(1) + ':D'
         }
 
       }
@@ -55,7 +54,7 @@ document.addEventListener('DOMContentLoaded', () => {
       console.log(dataSet);
       $('#dash1').DataTable({
         data: dataSet,
-        columns: columns,
+        columns: columns_Name,
       })
 
     })
