56886938f2dcc1573731cd0b79214ed7f966d8d5
Nouri T <zorisse@gmail.com>
2019-04-23 09:57:41 +0200

    done for user / week

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,78 @@
-document.addEventListener('DOMContentLoaded', () => {
+let semaines = [];
+for (let i = 0; i < 50; i++) {
+  semaines.push(i);
+}
 
+
+
+let columns = [{ title: "Dev" }]
+for (let i = 1; i < semaines.length; i++) {
+  columns.push({ title: `Week ${i}` })
+}
+// console.log('colonne', columns); ok ca marche 
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
   console.log('IronGenerator JS imported successfully!');
+
+
+  Promise.all([axios.get('/api/users'), axios.get('/api/dashboard')])
+
+    .then(([user, cal]) => {
+      // var dataSet = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []
+      // ];
+      var dataSet = [];
+      let users = user.data.map(e => e.username)
+      console.log('array users', users)
+
+
+      for (let index = 0; index < users.length - 1; index++) {
+
+        //ajoute le nom du USER en debut de l'array 
+        dataSet.push([`${users[index]}`])
+
+        // on filtre tout le travail pour un USER 
+        let oneUser = cal.data.calendar.filter(e => e.user.username === users[index])
+        console.log('test de la loop pour ' + users[index])
+        console.log(oneUser)
+
+        // Loop pour chaque week ajouter un string à DATASET [USER][0, .... -> 50]
+        for (let i = 1; i < 51; i++) {
+
+          let oneweek = oneUser.filter(e => e.week === i)
+
+          let times = 0
+          if (oneweek.length > 0) {
+            oneweek.forEach(e => times += e.time)
+            console.log('times ', times)
+            dataSet[index][i] = times.toFixed(2)
+          }
+
+        }
+
+      }
+
+      console.log(dataSet);
+
+
+
+
+
+
+
+
+      $('#example').DataTable({
+        data: dataSet,
+        columns: columns,
+      })
+
+    })
+
+
+
 
 }, false);
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const User = require('../models/User');
 const Project = require('../models/Project');
 const Calendar = require('../models/Calendar');
@@ -29,60 +29,81 @@ let isAdmin = (req, res, next) => {
 
 router.get('/', isAuthenticated, (req, res, next) => {
   Project.find()
-  .then(projects => {
-      res.render('home', {projects});
+    .then(projects => {
+      res.render('home', { projects });
     })
 });
 
 
-router.post('/calendar/new', isAuthenticated, (req, res, next)=> {
+router.post('/calendar/new', isAuthenticated, (req, res, next) => {
   let calendar = req.body
   calendar.user = req.user._id
   Calendar.create(calendar)
-  .then(_ => {
-    res.redirect('/')
-  })
+    .then(_ => {
+      res.redirect('/')
+    })
 })
 
 
 /* GET home page */
 router.get('/admin', isAuthenticated, isAdmin, (req, res, next) => {
   User.find()
-  .then(users => {
-  Project.find()
-  .then(projects => {
-      res.render('index', {users, projects});
+    .then(users => {
+      Project.find()
+        .then(projects => {
+          res.render('index', { users, projects });
+        })
     })
-  })
 });
 
+router.get('/api/users', isAuthenticated, isAdmin, (req, res, next) => {
+  User.find()
+    .then(users => {
+      res.send(users);
+    })
+});
+
+
 router.get('/users/new', (req, res, next) => {
-        res.render('newuser');
+  res.render('newuser');
 });
 
 router.get('/users/delete/:id', (req, res, next) => {
-  User.deleteOne({_id: req.params.id})
-  .then(_ => {
-    res.redirect('/');
-  })  
+  User.deleteOne({ _id: req.params.id })
+    .then(_ => {
+      res.redirect('/');
+    })
 });
 
 router.get('/users/archive/:id', (req, res, next) => {
-  User.updateOne({_id: req.params.id}, {$set: {hidden: true}})
-  .then(_ => {
-    res.redirect('/');
-  })  
+  User.updateOne({ _id: req.params.id }, { $set: { hidden: true } })
+    .then(_ => {
+      res.redirect('/');
+    })
 });
 
 router.post('/users/new', (req, res, next) => {
-  req.body.password=bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(bcryptSalt))
+  req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(bcryptSalt))
   req.body.intern = req.body.intern == "on"
   User.create(req.body)
-  .then(user=>{
-          res.redirect('/');
-  })
-  });
+    .then(user => {
+      res.redirect('/');
+    })
+});
 
+
+//DashBoard 
+router.get('/admin/dashboard', isAuthenticated, isAdmin, (req, res, next) => {
+  res.render('dashboard/dashboard')
+})
+router.get('/api/dashboard', isAuthenticated, isAdmin, (req, res, next) => {
+  Calendar.find()
+    .populate('user')
+    .populate('project')
+    .then(calendar => {
+      res.send({ calendar });
+    })
+});
 
 
 module.exports = router;
